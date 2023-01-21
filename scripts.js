9ccc6064ad2c3df28651ed4c63ed1f930f85ed63
Davi Barci <davibarci0208@gmail.com>
2023-01-21 00:39:23 -0300

    Vesão 3.0 - Adicionada a funcionalidade de recuperar e exibir as 100 últimas mensagens do servidor.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ let statusOnline;
 
 function usuarioOnline(){
     statusOnline = new Date();
+    console.log(`Usuário Online - ${statusOnline}`);
 }
 
 function usuarioOffline(){
@@ -17,9 +18,50 @@ function verificarStatus(){
     requisicao.catch(usuarioOffline);
 }
 
+function falhaMensagens(){
+    document.location.reload();
+}
+
+function sucessoMensagens(resposta) {
+    let mensagemAtual;
+    const containerMensagens = document.querySelector(".container-mensagens");
+    containerMensagens.innerHTML = "";
+    const lista = resposta.data;
+
+    for (let i = 0; i < lista.length; i++){
+        if (lista[i].type === "status"){
+            mensagemAtual = document.createElement("div");
+            mensagemAtual.classList.add("mensagens-status");
+            mensagemAtual.innerHTML = `<p><span>(${lista[i].time})</span>&nbsp;<b>${lista[i].from}</b>&nbsp;${lista[i].text}</p>`
+            containerMensagens.appendChild(mensagemAtual);
+        }else if (lista[i].type === "message"){
+            mensagemAtual = document.createElement("div");
+            mensagemAtual.classList.add("mensagens");
+            mensagemAtual.innerHTML = `<p><span>(${lista[i].time})</span>&nbsp;<b>${lista[i].from}&nbsp;</b>para&nbsp;<b>${lista[i].to}&nbsp;</b>: ${lista[i].text}</p>`
+            containerMensagens.appendChild(mensagemAtual);
+        }else if (lista[i].type === "private_message" && lista[i].to === nome){
+            mensagemAtual = document.createAttribute("div");
+            mensagemAtual.classList.add("mensagens-reservadas");
+            mensagemAtual.innerHTML = `<p><span>(${lista[i].time})</span>&nbsp;<b>${lista[i].from}&nbsp;</b>reservadamente para<b>&nbsp;${lista[i].to}</b>: ${lista[i].text}</p>`
+            containerMensagens.appendChild(mensagemAtual);
+        }
+    }
+
+    document.querySelector(".container-mensagens div:nth-last-child(1)").scrollIntoView();
+}
+
+function getMensagens(){
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promessa.then(sucessoMensagens);
+    promessa.catch(falhaMensagens);
+}
+
 function sucessoLogin(resposta) {
 	console.log(resposta.data);
+    getMensagens();
     setInterval(verificarStatus, 5000);
+    getMensagens();
+    setInterval(getMensagens, 3000);
 }
 
 function loginNegado(erro){
